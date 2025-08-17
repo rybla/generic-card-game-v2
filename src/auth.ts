74d41a231e3df87f6ac95cbfb1a101549c6360f0
Henry Blanchette <henry@rybl.net>
2025-08-17 16:29:24 -0400

    using next-auth v5 (beta) which is much simpler

--- src/auth.ts
+++ src/auth.ts
@@ -1,27 +1,12 @@
-import {
-    GetServerSidePropsContext,
-    NextApiRequest,
-    NextApiResponse,
-} from "next";
-import { getServerSession, NextAuthOptions, Session } from "next-auth";
+import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import env from "./env";
 
-export const authOptions: NextAuthOptions = {
+export const { auth, handlers, signIn, signOut } = NextAuth({
     providers: [
         Google({
             clientId: env.AUTH_GOOGLE_ID,
             clientSecret: env.AUTH_GOOGLE_SECRET,
         }),
     ],
-    secret: env.AUTH_SECRET,
-};
-
-export function auth(
-    ...args:
-        | [GetServerSidePropsContext["req"], GetServerSidePropsContext["res"]]
-        | [NextApiRequest, NextApiResponse]
-        | []
-): Promise<Session | null> {
-    return getServerSession(...args, authOptions);
-}
+});
